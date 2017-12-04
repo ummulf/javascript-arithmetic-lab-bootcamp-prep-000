645e102795cef35d48704e46fb8e4b85b98201a0
Ummul Fatima <ummulrizvi@yahoo.com>
2017-12-04 17:57:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function add5() {
 }
 
 function subtract5() {
-  number --= 5
+  number --= 4
 }
 
 function multiplyby5() {
-  number *= 5
+  number *= 50
 }
 
 function divideby5() {
-  number /= 5
+  number /= 2
 }

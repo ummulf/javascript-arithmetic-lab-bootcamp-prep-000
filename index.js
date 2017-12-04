eb7e843697c57d01ffee0b2389894d24ef20486d
Ummul Fatima <ummulrizvi@yahoo.com>
2017-12-04 17:42:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function divideBy3() {
 
 divideBy3()
 
-console.log(number) // 3.333333333335
+console.log(5) // 3.333333333335
 
 add5()
 
-console.log(number) // 8.333333333335
+console.log(10) // 8.333333333335
 
 // reset number
 number = 10
